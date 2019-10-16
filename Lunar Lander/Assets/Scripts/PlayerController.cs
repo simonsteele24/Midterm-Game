2dd5b2175c6a1bf546f4ee7c88f96b0939e317b6
Simon Steele <simon.steele@mymail.champlain.edu>
2019-10-16 12:16:05 -0400

    Added all essential physic engine scripts

---
+++ Lunar Lander/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿/*
+Author: Simon Steele
+Class: GPR-350-101
+Assignment: Lab 3
+Certification of Authenticity: We certify that this
+assignment is entirely our own work.
+*/
+
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Particle classes
+    private Particle2D particle;
+
+    // Floats
+    public float speed;
+    public float thrustSpeed;
+    public float startingVerticalVelocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particle = GetComponent<Particle2D>();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Has the D key been pressed?
+        if (Input.GetKey(KeyCode.D))
+        {
+        
+            // If yes, then add torque in the right direction
+            particle.ApplyTorque(new Vector2(-speed, 0), new Vector2(0, transform.position.y + particle.boxDimensions.y));
+
+        }
+
+        // Has the A key been pressed?
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            // If yes, then add torque in the left direction
+            particle.ApplyTorque(new Vector2(speed, 0), new Vector2(0, transform.position.y + particle.boxDimensions.y));
+
+        }
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            particle.AddForce(new Vector2(0, thrustSpeed));
+        }
+    }
+}
---
+++ Lunar Lander/Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager ui;
+    public Text horizontalText;
+    public Text verticalText;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ui = this;
+    }
+
+    public void ChangeVerticalSpeedText(string newText)
+    {
+        horizontalText.text = newText;
+    }
+
+    public void ChangeHorizontalSpeedText(string newText)
+    {
+        horizontalText.text = newText;
+    }
+}
